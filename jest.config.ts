1e992ffd152311bd7176e9e9f3d182c2fe777be6
Qwertic <vodaregi@gmail.com>
2024-09-10 18:20:48 +0200

    rm unused code

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-};
-
-export default config;
